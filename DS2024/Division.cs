ca4a11734e52052dea41bbcc8fef300d5a4a188a
Sangkon Han <sigmadream@gmail.com>
2024-08-13 04:17:36 +0900

    feat: 프로젝트 설정

--- DS2024/Division.cs
+++ DS2024/Division.cs
@@ -4,7 +4,7 @@
     {
         public static decimal Divide(int dividend, int divisor)
         {
-            decimal quotient = dividend / divisor;
+            decimal quotient = (decimal)dividend / divisor;
             return quotient;
         }
     }
