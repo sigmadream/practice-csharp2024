3dce3d9b91c615332ce882795501091255eaaeba
Sangkon Han <sigmadream@gmail.com>
2024-08-13 04:05:37 +0900

    Add project files.

---
+++ DS2024/Division.cs
@@ -0,0 +1,11 @@
+﻿namespace DS2024
+{
+    public class Division
+    {
+        public static decimal Divide(int dividend, int divisor)
+        {
+            decimal quotient = dividend / divisor;
+            return quotient;
+        }
+    }
+}
