500f3fffe6639cef7594340cc3ac7f55569604a2
Sangkon Han <sigmadream@gmail.com>
2024-08-29 01:49:54 +0900

    // wip: try 6

---
+++ DS2024/Sort/Comparison/InsertionSort.cs
@@ -0,0 +1,21 @@
+﻿namespace DS2024.Sort.Comparison;
+
+public class InsertionSort<T> : IComparisonSorter<T>
+{
+    public void Sort(T[] array, IComparer<T> comparer)
+    {
+        for (var i = 1; i < array.Length; i++)
+        {
+            var key = array[i];
+            var j = i - 1;
+
+            while (j >= 0 && comparer.Compare(array[j], key) > 0)
+            {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = key;
+        }
+    }
+}
