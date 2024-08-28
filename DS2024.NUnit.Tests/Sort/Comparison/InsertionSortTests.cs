2ba71b44ddf15d377da1f333bd654de66a9f63be
Sangkon Han <sigmadream@gmail.com>
2024-08-29 01:54:10 +0900

    // wip: try7

--- DS2024.NUnit.Tests/Sort/Comparison/InsertionSortTests.cs
+++ DS2024.NUnit.Tests/Sort/Comparison/InsertionSortTests.cs
@@ -4,27 +4,12 @@ namespace DS2024.NUnit.Tests.Sort.Comparison;
 
 public class InsertionSortTests
 {
-    private static (int[] correctArray, int[] testArray) GetArrays(int n)
-    {
-        var testArr = new int[n];
-        var correctArray = new int[n];
-
-        for (var i = 0; i < n; i++)
-        {
-            var t = TestContext.CurrentContext.Random.Next(1_000_000);
-            testArr[i] = t;
-            correctArray[i] = t;
-        }
-
-        return (correctArray, testArr);
-    }
-
     [Test]
     public static void ArraySorted([Random(0, 1000, 100, Distinct = true)] int n)
     {
         // Arrange
         var sorter = new InsertionSort<int>();
-        var (correctArray, testArray) = GetArrays(n);
+        var (correctArray, testArray) = RandomArray.GetArrays(n);
 
         // Act
         sorter.Sort(testArray, Comparer<int>.Create((x, y) => x.CompareTo(y)));
